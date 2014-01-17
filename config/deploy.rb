ad6990c978bb08fec77a09c82ff9a974f57700d6
allansideas <allansideas@gmail.com>
2014-01-17 16:32:34 +0100

    mod deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,22 @@
-set :application, "wowwi"
-set :repository,  "git@github.com:allansideas/#{application}.git"
+set :application, "woww"
+set :repository,  "git@github.com:allansideas/wpww.git"
 set :use_sudo,    false
+set :rake, "bundle exec rake"
+set :rvm_ruby_string, "ruby-2.0.0-p195"
+
 
 set :normalize_asset_timestamps, false
-default_run_options[:shell] = '/bin/bash --login'
 set :scm, :git
+
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 task :staging do
   set :user,      "woww"
   set :domain,    "woww.instantiate.me"
-  set :branch,    "staging"
+  set :branch,    "master"
   set :rails_env, "staging"
   set :deploy_to, "/home/#{user}/api/"
   set :bundle_without, [:development, :test]
@@ -23,7 +28,7 @@ end
 
 namespace :site do
   task :symlink do
-    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   task :reset_db do
     rake_task("db:reset")
@@ -39,7 +44,12 @@ namespace :rake do
   end
 end
 
+before 'deploy', 'rvm:install_rvm'  # update RVM 
+before 'deploy', 'rvm:install_ruby' # install Ruby and create gemset (both if missing) 
+
 require "bundler/capistrano"
+require 'rvm/capistrano'
+
 after "deploy:restart", "deploy:cleanup"
 after "deploy:update_code", "site:symlink"
 
